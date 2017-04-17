e669b33f7db10a3c242f71411af2bd69d5cd3f4f
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-17 19:25:19 +0900

    cmd/vmrun: add snapshot test
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- cmd/vmrun/main.go
+++ cmd/vmrun/main.go
@@ -4,7 +4,11 @@
 
 package main
 
-import "github.com/go-vm/vmware/fusion"
+import (
+	"log"
+
+	"github.com/go-vm/vmware/fusion"
+)
 
 const vmwarevmPath = "/Volumes/APFS/VirtualMachine/macOS-10.12.vmwarevm/macOS-10.12.vmx"
 
@@ -16,4 +20,33 @@ func main() {
 	if err := fusion.Stop(vmwarevmPath, true); err != nil {
 		panic(err.Error())
 	}
+
+	list, num, err := fusion.ListSnapshots(vmwarevmPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Printf("list: %T => %+v\n", list, list)
+	log.Printf("num: %T => %+v\n", num, num)
+
+	if err := fusion.Snapshot(vmwarevmPath, "testSnapshot"); err != nil {
+		panic(err.Error())
+	}
+
+	list2, num2, err := fusion.ListSnapshots(vmwarevmPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Printf("list: %T => %+v\n", list2, list2)
+	log.Printf("num: %T => %+v\n", num2, num2)
+
+	if err := fusion.DeleteSnapshot(vmwarevmPath, "testSnapshot", true); err != nil {
+		panic(err.Error())
+	}
+
+	list3, num3, err := fusion.ListSnapshots(vmwarevmPath)
+	if err != nil {
+		panic(err.Error())
+	}
+	log.Printf("list: %T => %+v\n", list3, list3)
+	log.Printf("num: %T => %+v\n", num3, num3)
 }
