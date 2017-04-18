98150348b86fe55ef98a36a9d8671d8d5c066733
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-18 13:26:16 +0900

    fusion: move fusion package to vmware package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

---
+++ fusion.go
@@ -0,0 +1,103 @@
+// Copyright 2017 The go-vm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vmware
+
+import (
+	"github.com/go-vm/vmware/vmrun"
+)
+
+const fusionApp = "fusion"
+
+// Fusion represents a VMware Fusion application.
+type Fusion struct {
+	vmx string
+}
+
+// NewFusion return the new Fusion.
+func NewFusion(vmx string) *Fusion {
+	return &Fusion{
+		vmx: vmx,
+	}
+}
+
+// Start start a VM or Team.
+func (f *Fusion) Start(gui bool) error {
+	return vmrun.Start(fusionApp, f.vmx, gui)
+}
+
+// ShutDown wrap of stop command with hard.
+func (f *Fusion) ShutDown() error {
+	return vmrun.Stop(fusionApp, f.vmx, true)
+}
+
+// Halt wrap of stop command with soft.
+func (f *Fusion) Halt() error {
+	return vmrun.Stop(fusionApp, f.vmx, false)
+}
+
+// Reset reset a VM or Team.
+func (f *Fusion) Reset() error {
+	return vmrun.Reset(fusionApp, f.vmx, true)
+}
+
+// Restart restart a VM uses wrap of reset command with soft.
+func (f *Fusion) Restart() error {
+	return vmrun.Reset(fusionApp, f.vmx, false)
+}
+
+// Suspend Suspend a VM or Team.
+func (f *Fusion) Suspend(hard bool) error {
+	return vmrun.Suspend(fusionApp, f.vmx, hard)
+}
+
+// Pause pause a VM.
+func (f *Fusion) Pause() error {
+	return vmrun.Pause(fusionApp, f.vmx)
+}
+
+// Unpause unpause a VM.
+func (f *Fusion) Unpause(vmx string) error {
+	return vmrun.Unpause(fusionApp, f.vmx)
+}
+
+// ListSnapshots list all snapshots in a VM.
+func (f *Fusion) ListSnapshots() ([]string, int, error) {
+	return vmrun.ListSnapshots(fusionApp, f.vmx)
+}
+
+// Snapshot create a snapshot of a VM.
+func (f *Fusion) Snapshot(snapshotName string) error {
+	return vmrun.Snapshot(fusionApp, f.vmx, snapshotName)
+}
+
+// DeleteSnapshot remove a snapshot from a VM.
+func (f *Fusion) DeleteSnapshot(snapshotName string, deleteChildren bool) error {
+	return vmrun.DeleteSnapshot(fusionApp, f.vmx, snapshotName, deleteChildren)
+}
+
+// RevertToSnapshot set VM state to a snapshot.
+func (f *Fusion) RevertToSnapshot(snapshotName string) error {
+	return vmrun.RevertToSnapshot(fusionApp, f.vmx, snapshotName)
+}
+
+// RunProgramInGuest run a program in Guest OS.
+func (f *Fusion) RunProgramInGuest(auth vmrun.Auth, config vmrun.RunProgramInGuestConfig, cmdPath string, cmdArgs ...string) error {
+	return vmrun.RunProgramInGuest(fusionApp, f.vmx, auth, config, cmdPath, cmdArgs...)
+}
+
+// FileExistsInGuest check if a file exists in Guest OS.
+func (f *Fusion) FileExistsInGuest(auth vmrun.Auth, filename string) bool {
+	return vmrun.FileExistsInGuest(fusionApp, f.vmx, auth, filename)
+}
+
+// DirectoryExistsInGuest check if a directory exists in Guest OS.
+func (f *Fusion) DirectoryExistsInGuest(auth vmrun.Auth, dir string) bool {
+	return vmrun.DirectoryExistsInGuest(fusionApp, f.vmx, auth, dir)
+}
+
+// SetSharedFolderState modify a Host-Guest shared folder.
+func (f *Fusion) SetSharedFolderState(auth vmrun.Auth, shareName, hostPath string, writable bool) bool {
+	return vmrun.SetSharedFolderState(fusionApp, f.vmx, auth, shareName, hostPath, writable)
+}
--- fusion/README.md
+++
@@ -1,8 +0,0 @@
-# fusion
-
-A package fusion implements a vmrun command wrapper of managing the VMware Fusion application.
-
-[![godoc.org][godoc-badge]][godoc]
-
-[godoc]: https://godoc.org/github.com/go-vm/vmware/fusion
-[godoc-badge]: https://img.shields.io/badge/godoc-reference-4F73B3.svg?style=flat-square&label=%20godoc.org
