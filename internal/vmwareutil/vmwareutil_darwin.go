a70eb6e573da3aef2d3279125cc1115b0d81d703
Koichi Shiraishi <zchee.io@gmail.com>
2017-04-18 13:24:17 +0900

    vmwareutil: move util to internal/vmwareutil package
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- internal/vmwareutil/vmwareutil_darwin.go
+++ internal/vmwareutil/vmwareutil_darwin.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package vmware
+package vmwareutil
 
 // vmwareDir is default directory path of vmware commands to fallback when it is not on path.
-var vmwareDir = "/usr/bin"
+var vmwareDir = "/Applications/VMware Fusion.app/Contents/Library"
